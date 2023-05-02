3a498b6d0bbd1c48115b01b0a0d8e644cf6cea24
SEUNGJUN JEONG <jsj9620@hufs.ac.kr>
2023-05-02 17:41:25 +0900

    Chore: init tailwindcss

---
+++ front/front/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      red: "#BB2649",
+      green: "#26BB98",
+      "light-blue": "#EEF1F4",
+      white: "#FFFFFF",
+      fontgray: "#AAAAAA",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Noto Sans KR"],
+      },
+    },
+  },
+  plugins: [],
+};
