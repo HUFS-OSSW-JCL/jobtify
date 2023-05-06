2809888141fe102bf48dc7b603ff86989eb99e6a
neomujua <neomujua@kakao.com>
2023-05-06 17:00:48 +0900

    firestore test

--- server/init_firebase.py
+++ server/init_firebase.py
@@ -8,3 +8,10 @@ cred = credentials.Certificate('./env/jobtify-jcl-firebase-adminsdk-486oy-725ae3
 app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+
+doc_ref = db.collection(u'users').document(u'alovelace')
+doc_ref.set({
+    u'first': u'Ada',
+    u'last': u'Lovelace',
+    u'born': 1815
+})
