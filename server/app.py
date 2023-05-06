5a5e60db2da7ba3e792c7743d9fed6e3a9848a41
neomujua <neomujua@kakao.com>
2023-05-07 03:30:14 +0900

    get request test

--- server/app.py
+++ server/app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return "<h1>Hello World!</h1>"
+
+@app.route('/jobtify/<jobtify_text>')
+def jobtify(jobtify_text):
+    return jobtify_text
