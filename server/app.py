e89580c8d19e6a99f4a0df3f1a271e1edc1a0b53
neomujua <neomujua@kakao.com>
2023-05-09 12:57:42 +0900

    tmp

--- server/app.py
+++ server/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import requests
+from firebase_admin import auth
 
 app = Flask(__name__)
 
@@ -15,4 +17,20 @@ def jobtify2(fname, keywords):
     print(fname)
     print(keywords.split("=="))
     return fname
-    
+
+@app.route('/user_id', methods=['POST'])
+def user_post():
+    params = json.loads(request.get_data(), encoding='utf-8')
+    if len(params) == 0:
+        return 'No parameter'
+
+    params_str = ''
+    for key in params.keys():
+        params_str += 'key: {}, value: {}<br>'.format(key, params[key])
+    return params_str
+
+@app.route('/test')
+def test():
+    uid
+    user = auth.get_user(uid)
+# user_id / keywords / location / crawl
