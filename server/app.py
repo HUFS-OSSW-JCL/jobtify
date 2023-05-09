66ecf46d1110f990adfebbb1f74267414681cc6c
Wonjun Lee <31210124+workoutplz@users.noreply.github.com>
2023-05-10 02:31:00 +0900

    post handle test

--- server/app.py
+++ server/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
-import requests
+from flask import Flask, request
 from firebase_admin import auth
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    print("test2")
     return "<h1>Hello World!</h1>"
 
 @app.route('/jobtify/<jobtify_text>')
@@ -18,19 +18,10 @@ def jobtify2(fname, keywords):
     print(keywords.split("=="))
     return fname
 
-@app.route('/user_id', methods=['POST'])
-def user_post():
-    params = json.loads(request.get_data(), encoding='utf-8')
-    if len(params) == 0:
-        return 'No parameter'
-
-    params_str = ''
-    for key in params.keys():
-        params_str += 'key: {}, value: {}<br>'.format(key, params[key])
-    return params_str
-
-@app.route('/test')
-def test():
-    uid
-    user = auth.get_user(uid)
 # user_id / keywords / location / crawl
+@app.route('/json_test', methods=['POST'])
+def handle_json():
+    data = request.json
+    user_id = data.get('user_id')
+    print(user_id)
+    return user_id
