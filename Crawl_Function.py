ca951a900021e1a20683a6eef55e1c34deea2b0f
CHOI INSEONG <cINSEONG0625@gmail.com>
2023-05-12 04:54:45 +0900

    function

---
+++ Crawl_Function.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from fake_useragent import  UserAgent
+import time
+
