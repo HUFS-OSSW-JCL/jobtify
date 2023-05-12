d5013d287acca934cd205973ada4fc1b5f798a68
CHOI_INSEONG <cINSEONG0625@gmail.com>
2023-05-12 22:37:49 +0900

    a

--- Incruit.py
+++ Incruit.py
@@ -1,2 +1,9 @@
 import Crawl_Function
 from selenium.webdriver.common.by import By
+from selenium import webdriver
+
+def SearchJob(keyword):
+    incruit = Crawl_Function.Crawler("https://job.incruit.com/jobdb_list/searchjob.asp?ct=3&ty=1&cd=149")
+    incruit.OpenSite()
+    incruit.driver.find_
+    incruit.Search(keyword,  "//*[@id=\"kw\"]", "//*[@id=\"kw\"]" )
