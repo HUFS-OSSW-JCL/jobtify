8c49c15ebe8aaff49db4ad2d67712ca31217ef98
CHOI-INSEONG <126836702+CHOI-INSEONG@users.noreply.github.com>
2023-05-16 20:27:18 +0900

    Crawl from Wanted

--- Crawling/wanted.py
+++ Crawling/wanted.py
@@ -1,6 +1,7 @@
 import Crawl_Function
 from selenium.webdriver.common.by import By
 
+
 def SearchJob(keyword):
     wanted = Crawl_Function.Crawler("https://www.wanted.co.kr/")
     wanted.OpenSite()
