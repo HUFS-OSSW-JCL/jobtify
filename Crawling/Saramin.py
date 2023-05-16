885137f6e2fb4085e985baf09d1c60e5e4fb17e8
CHOI INSEONG <cINSEONG0625@gmail.com>
2023-05-16 20:20:44 +0900

    Crawl from Saramin

--- Crawling/Saramin.py
+++ Crawling/Saramin.py
@@ -1,17 +1,15 @@
 import Crawl_Function
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+import time
 
 def SearchJob(keyword):
     saramin = Crawl_Function.Crawler("https://www.saramin.co.kr/zf_user/")
     saramin.OpenSite()
     saramin.Search(keyword,"//*[@id=\"sri_header\"]/div[1]/div[1]", "//*[@id=\"ipt_keyword_recruit\"]")
-    saramin.driver.find_elements(By.XPATH, "//*[@id=\"content\"]/ul[1]/li[2]/a").click()
-    job_lists = saramin.GetJobInfo("#recruit_info_list > div.content", "div")
-    for job in job_lists:
-        job_title = job.find_element(By.CSS_SELECTOR, "div.area_job > h2")
-        job_company = job.find_element(By.CSS_SELECTOR, "div.area_corp > strong")
-        saramin.job_list.append([job_title.text, job_company.text])
+    saramin.driver.find_element(By.XPATH, "//*[@id=\"content\"]/ul[1]/li[2]/a").click()
+    time.sleep(5)
+    saramin.GetJobInfo("#recruit_info_list > div.content", "div")
+    saramin.ReturnList("div.area_job > h2", "div.area_corp > strong", "div.area_job > div.job_date > span", "div.area_job > h2 > a")
 
     return saramin.job_list
