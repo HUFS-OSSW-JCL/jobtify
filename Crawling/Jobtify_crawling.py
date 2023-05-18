567b23817bfbe872271132e4242746daef3a0580
CHOI INSEONG <cINSEONG0625@gmail.com>
2023-05-18 20:19:29 +0900

    main Python file

--- Crawling/Jobtify_crawling.py
+++ Crawling/Jobtify_crawling.py
@@ -2,7 +2,30 @@ import Saramin
 import JobKorea
 import wanted
 import Incruit
+import ray
+import threading
+
+def siteFilter(sitename, keyword):
+    if sitename == "인크루트":
+        result = Incruit.SearchJob(keyword)
+    elif sitename == "잡코리아":
+        result = JobKorea.SearchJob(keyword)
+    elif sitename == "사람인":
+        result = Saramin.JobKorea.SearchJob(keyword)
+    elif sitename == "원티드":
+        result = wanted.SearchJob(keyword)
+    print(result)
 
 if __name__ == "__main__":
-    job_list = Incruit.SearchJob("개발자")
-    print(job_list)
+    sitelist = ["잡코리아", "인크루트"]
+    keys = list(input().split())
+
+    threads = []
+    for site in sitelist:
+        for key in keys:
+            t = threading.Thread(target = siteFilter, args = (site, key))
+            t.start()
+            threads.append(t)
+
+    for t in threads:
+        t.join()
