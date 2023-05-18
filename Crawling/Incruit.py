0df31a9354441184a9c0bee0043e65dd87db11f8
CHOI INSEONG <cINSEONG0625@gmail.com>
2023-05-18 20:20:51 +0900

    Crawl from Incruit

--- Crawling/Incruit.py
+++ Crawling/Incruit.py
@@ -6,10 +6,9 @@ import time
 def SearchJob(keyword):
     incruit = Crawl_Function.Crawler("https://job.incruit.com/jobdb_list/searchjob.asp?ct=3&ty=1&cd=149")
     incruit.OpenSite()
-    incruit.driver.find_element(By.CSS_SELECTOR, "#dropFirstList3").click()
-    incruit.driver.find_element(By.CSS_SELECTOR, "#crr_list").click()
-    incruit.driver.find_element(By.CSS_SELECTOR, "#crr_list > option:nth-child(3)").click()
-    time.sleep(2)
+    incruit.click("#dropFirstList3")
+    incruit.click("#crr_list")
+    incruit.click("#crr_list > option:nth-child(3)")
     incruit.Search(keyword, "//*[@id=\"txtSearchKw\"]", "//*[@id=\"txtSearchKw\"]")
     time.sleep(2)
     filter = incruit.driver.find_element(By.CSS_SELECTOR, "#divAutoComplete > ul")
@@ -20,7 +19,8 @@ def SearchJob(keyword):
         if "직접검색" in v.text:
             v.click()
     time.sleep(2)
-    incruit.driver.find_element(By.XPATH, "//*[@id=\"SearchResultCount\"]").click()
+    incruit.click("#SearchResultCount")
+  
     incruit.GetJobInfo("#incruit_contents > div > div > div.cBbslist_contenst", "li")
     job_lists = incruit.ReturnList("div.cell_mid > div.cl_top","div.cell_first > div.cl_top > a","div.cell_last > div.cl_btm > span:nth-child(1)", "div.cell_mid > div.cl_top > a")
     return job_lists
