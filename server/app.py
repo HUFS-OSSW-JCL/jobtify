6f448c0e3e97559cb95e49d4ed2dfdcc65df373a
Wonjun Lee <31210124+workoutplz@users.noreply.github.com>
2023-05-22 20:11:36 +0900

    register information via uid

--- server/app.py
+++ server/app.py
@@ -41,5 +41,19 @@ def handle_json():
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, X-Requested-With"
     return response
 
+@app.route('/register', methods=['POST'])
+def register_user():
+    data = request.json
+    uid = data.get('uid')
+    keywords = data.get('keywords')
+    country = data.get('country')
+    sites = data.get('sites')
+    response = make_response(jsonify({'status':'good'}))
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, X-Requested-With"
+    print(uid, keywords, country, sites)
+    return response
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
