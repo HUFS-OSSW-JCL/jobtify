a67d406200d8d3501483e6d19c4805bc90eebdd5
SEUNGJUN JEONG <jsj9620@hufs.ac.kr>
2023-06-13 00:14:54 +0900

    Fix: 드롭다운 이슈 수정

--- front/src/components/DropDown.jsx
+++ front/src/components/DropDown.jsx
@@ -1,9 +1,9 @@
 const DropDown = ({ options, comboBoxHandler, onClick }) => {
   return (
     <div className="z-30 bg-white block ml-auto mr-auto list-none mt-[-1px] p-1 border border-gray-300">
-      {options.map((x, i, key) =>
+      {options.map((x, i) =>
         x.indexOf(comboBoxHandler) > -1 ? (
-          <div key={key} onClick={() => onClick(i)}>
+          <div key={i} onClick={() => onClick(i)}>
             {x}
           </div>
         ) : null
